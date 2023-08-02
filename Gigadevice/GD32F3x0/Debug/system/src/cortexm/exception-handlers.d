5bed66c240d1cddec576f6421f0bf2a96b4d1eac
Ivanov Dmitriy <84959980+IvanovDmitriy09@users.noreply.github.com>
2023-08-02 13:47:32 +0300

    add template for gd32f3x0

---
+++ Gigadevice/GD32F3x0/Debug/system/src/cortexm/exception-handlers.d
@@ -0,0 +1,67 @@
+system/src/cortexm/exception-handlers.o: \
+ ../system/src/cortexm/exception-handlers.c \
+ ../system/inc/cortexm/exception-handlers.h \
+ ../system/inc/cmsis/cmsis_device.h ../system/inc/cmsis/gd32f3x0.h \
+ ../system/inc/cmsis/core_cm4.h ../system/inc/cmsis/core_cmInstr.h \
+ ../system/inc/cmsis/core_cmFunc.h ../system/inc/cmsis/core_cm4_simd.h \
+ ../system/inc/cmsis/system_gd32f3x0.h \
+ ../system/inc/cmsis/gd32f3x0_libopt.h \
+ ../system/inc/gd32f3x0/gd32f3x0_adc.h ../system/inc/cmsis/gd32f3x0.h \
+ ../system/inc/gd32f3x0/gd32f3x0_crc.h \
+ ../system/inc/gd32f3x0/gd32f3x0_ctc.h \
+ ../system/inc/gd32f3x0/gd32f3x0_dbg.h \
+ ../system/inc/gd32f3x0/gd32f3x0_dma.h \
+ ../system/inc/gd32f3x0/gd32f3x0_exti.h \
+ ../system/inc/gd32f3x0/gd32f3x0_fmc.h \
+ ../system/inc/gd32f3x0/gd32f3x0_gpio.h \
+ ../system/inc/gd32f3x0/gd32f3x0_syscfg.h \
+ ../system/inc/gd32f3x0/gd32f3x0_i2c.h \
+ ../system/inc/gd32f3x0/gd32f3x0_fwdgt.h \
+ ../system/inc/gd32f3x0/gd32f3x0_pmu.h \
+ ../system/inc/gd32f3x0/gd32f3x0_rcu.h \
+ ../system/inc/gd32f3x0/gd32f3x0_rtc.h \
+ ../system/inc/gd32f3x0/gd32f3x0_spi.h \
+ ../system/inc/gd32f3x0/gd32f3x0_timer.h \
+ ../system/inc/gd32f3x0/gd32f3x0_usart.h \
+ ../system/inc/gd32f3x0/gd32f3x0_wwdgt.h \
+ ../system/inc/gd32f3x0/gd32f3x0_misc.h \
+ ../system/inc/gd32f3x0/gd32f3x0_tsi.h \
+ ../system/inc/gd32f3x0/gd32f3x0_cec.h \
+ ../system/inc/gd32f3x0/gd32f3x0_cmp.h \
+ ../system/inc/gd32f3x0/gd32f3x0_dac.h ../system/inc/arm/semihosting.h \
+ ../system/inc/diag/trace.h
+../system/inc/cortexm/exception-handlers.h:
+../system/inc/cmsis/cmsis_device.h:
+../system/inc/cmsis/gd32f3x0.h:
+../system/inc/cmsis/core_cm4.h:
+../system/inc/cmsis/core_cmInstr.h:
+../system/inc/cmsis/core_cmFunc.h:
+../system/inc/cmsis/core_cm4_simd.h:
+../system/inc/cmsis/system_gd32f3x0.h:
+../system/inc/cmsis/gd32f3x0_libopt.h:
+../system/inc/gd32f3x0/gd32f3x0_adc.h:
+../system/inc/cmsis/gd32f3x0.h:
+../system/inc/gd32f3x0/gd32f3x0_crc.h:
+../system/inc/gd32f3x0/gd32f3x0_ctc.h:
+../system/inc/gd32f3x0/gd32f3x0_dbg.h:
+../system/inc/gd32f3x0/gd32f3x0_dma.h:
+../system/inc/gd32f3x0/gd32f3x0_exti.h:
+../system/inc/gd32f3x0/gd32f3x0_fmc.h:
+../system/inc/gd32f3x0/gd32f3x0_gpio.h:
+../system/inc/gd32f3x0/gd32f3x0_syscfg.h:
+../system/inc/gd32f3x0/gd32f3x0_i2c.h:
+../system/inc/gd32f3x0/gd32f3x0_fwdgt.h:
+../system/inc/gd32f3x0/gd32f3x0_pmu.h:
+../system/inc/gd32f3x0/gd32f3x0_rcu.h:
+../system/inc/gd32f3x0/gd32f3x0_rtc.h:
+../system/inc/gd32f3x0/gd32f3x0_spi.h:
+../system/inc/gd32f3x0/gd32f3x0_timer.h:
+../system/inc/gd32f3x0/gd32f3x0_usart.h:
+../system/inc/gd32f3x0/gd32f3x0_wwdgt.h:
+../system/inc/gd32f3x0/gd32f3x0_misc.h:
+../system/inc/gd32f3x0/gd32f3x0_tsi.h:
+../system/inc/gd32f3x0/gd32f3x0_cec.h:
+../system/inc/gd32f3x0/gd32f3x0_cmp.h:
+../system/inc/gd32f3x0/gd32f3x0_dac.h:
+../system/inc/arm/semihosting.h:
+../system/inc/diag/trace.h:
---
+++ Gigadevice/GD32F3x0/system/inc/cmsis/README_DEVICE.txt
@@ -0,0 +1,11 @@
+The "gd32f3x0.h" and "system_gd32f3x0.h" files are provided 
+only as a functional sample.
+
+For real applications they must be replaced by the vendor provided files.
+
+Extensions to the ARM CMSIS files:
+
+- the file "cmsis_device.h" was added, as a portable method to include the 
+vendor device header file in library sources. 
+
+  
