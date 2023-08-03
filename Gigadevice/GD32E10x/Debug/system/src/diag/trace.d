54d5364f3cc15b0b7d3ea62c446bcc24bddc3ad0
Ivanov Dmitriy <84959980+IvanovDmitriy09@users.noreply.github.com>
2023-08-03 14:30:07 +0300

    add template for gd32e10x

---
+++ Gigadevice/GD32E10x/Debug/system/src/diag/trace.d
@@ -0,0 +1,3 @@
+system/src/diag/trace.o: ../system/src/diag/trace.c \
+ ../system/inc/diag/trace.h
+../system/inc/diag/trace.h:
---
+++ Gigadevice/GD32E10x/Debug/system/src/gd32e10x/gd32e10x_i2c.d
@@ -0,0 +1,54 @@
+system/src/gd32e10x/gd32e10x_i2c.o: ../system/src/gd32e10x/gd32e10x_i2c.c \
+ ../system/inc/gd32e10x/gd32e10x_i2c.h ../system/inc/cmsis/gd32e10x.h \
+ ../system/inc/cmsis/core_cm4.h ../system/inc/cmsis/core_cmInstr.h \
+ ../system/inc/cmsis/core_cmFunc.h ../system/inc/cmsis/core_cm4_simd.h \
+ ../system/inc/cmsis/system_gd32e10x.h \
+ ../system/inc/cmsis/gd32e10x_libopt.h \
+ ../system/inc/gd32e10x/gd32e10x_rcu.h \
+ ../system/inc/gd32e10x/gd32e10x_adc.h \
+ ../system/inc/gd32e10x/gd32e10x_crc.h \
+ ../system/inc/gd32e10x/gd32e10x_ctc.h \
+ ../system/inc/gd32e10x/gd32e10x_dac.h \
+ ../system/inc/gd32e10x/gd32e10x_dbg.h \
+ ../system/inc/gd32e10x/gd32e10x_dma.h \
+ ../system/inc/gd32e10x/gd32e10x_exti.h \
+ ../system/inc/gd32e10x/gd32e10x_fmc.h \
+ ../system/inc/gd32e10x/gd32e10x_fwdgt.h \
+ ../system/inc/gd32e10x/gd32e10x_gpio.h \
+ ../system/inc/gd32e10x/gd32e10x_pmu.h \
+ ../system/inc/gd32e10x/gd32e10x_bkp.h \
+ ../system/inc/gd32e10x/gd32e10x_rtc.h \
+ ../system/inc/gd32e10x/gd32e10x_spi.h \
+ ../system/inc/gd32e10x/gd32e10x_timer.h \
+ ../system/inc/gd32e10x/gd32e10x_usart.h \
+ ../system/inc/gd32e10x/gd32e10x_wwdgt.h \
+ ../system/inc/gd32e10x/gd32e10x_misc.h \
+ ../system/inc/gd32e10x/gd32e10x_exmc.h
+../system/inc/gd32e10x/gd32e10x_i2c.h:
+../system/inc/cmsis/gd32e10x.h:
+../system/inc/cmsis/core_cm4.h:
+../system/inc/cmsis/core_cmInstr.h:
+../system/inc/cmsis/core_cmFunc.h:
+../system/inc/cmsis/core_cm4_simd.h:
+../system/inc/cmsis/system_gd32e10x.h:
+../system/inc/cmsis/gd32e10x_libopt.h:
+../system/inc/gd32e10x/gd32e10x_rcu.h:
+../system/inc/gd32e10x/gd32e10x_adc.h:
+../system/inc/gd32e10x/gd32e10x_crc.h:
+../system/inc/gd32e10x/gd32e10x_ctc.h:
+../system/inc/gd32e10x/gd32e10x_dac.h:
+../system/inc/gd32e10x/gd32e10x_dbg.h:
+../system/inc/gd32e10x/gd32e10x_dma.h:
+../system/inc/gd32e10x/gd32e10x_exti.h:
+../system/inc/gd32e10x/gd32e10x_fmc.h:
+../system/inc/gd32e10x/gd32e10x_fwdgt.h:
+../system/inc/gd32e10x/gd32e10x_gpio.h:
+../system/inc/gd32e10x/gd32e10x_pmu.h:
+../system/inc/gd32e10x/gd32e10x_bkp.h:
+../system/inc/gd32e10x/gd32e10x_rtc.h:
+../system/inc/gd32e10x/gd32e10x_spi.h:
+../system/inc/gd32e10x/gd32e10x_timer.h:
+../system/inc/gd32e10x/gd32e10x_usart.h:
+../system/inc/gd32e10x/gd32e10x_wwdgt.h:
+../system/inc/gd32e10x/gd32e10x_misc.h:
+../system/inc/gd32e10x/gd32e10x_exmc.h:
