18876f5fae9e62c5f63568c77417bdd10d56be4a
Ivanov Dmitriy <84959980+IvanovDmitriy09@users.noreply.github.com>
2023-08-07 14:45:21 +0300

    add template for gd32c11x

---
+++ Gigadevice/GD32C11x/Debug/system/src/cmsis/vectors_gd32c11x.d
@@ -0,0 +1,4 @@
+system/src/cmsis/vectors_gd32c11x.o: \
+ ../system/src/cmsis/vectors_gd32c11x.c \
+ ../system/inc/cortexm/exception-handlers.h
+../system/inc/cortexm/exception-handlers.h:
---
+++ Gigadevice/GD32C11x/Debug/system/src/gd32c11x/gd32c11x_crc.d
@@ -0,0 +1,56 @@
+system/src/gd32c11x/gd32c11x_crc.o: ../system/src/gd32c11x/gd32c11x_crc.c \
+ ../system/inc/gd32c11x/gd32c11x_crc.h ../system/inc/cmsis/gd32c11x.h \
+ ../system/inc/cmsis/core_cm4.h ../system/inc/cmsis/core_cmInstr.h \
+ ../system/inc/cmsis/core_cmFunc.h ../system/inc/cmsis/core_cm4_simd.h \
+ ../system/inc/cmsis/system_gd32c11x.h \
+ ../system/inc/cmsis/gd32c11x_libopt.h \
+ ../system/inc/gd32c11x/gd32c11x_rcu.h \
+ ../system/inc/gd32c11x/gd32c11x_adc.h \
+ ../system/inc/gd32c11x/gd32c11x_can.h \
+ ../system/inc/gd32c11x/gd32c11x_ctc.h \
+ ../system/inc/gd32c11x/gd32c11x_dac.h \
+ ../system/inc/gd32c11x/gd32c11x_dbg.h \
+ ../system/inc/gd32c11x/gd32c11x_dma.h \
+ ../system/inc/gd32c11x/gd32c11x_exti.h \
+ ../system/inc/gd32c11x/gd32c11x_fmc.h \
+ ../system/inc/gd32c11x/gd32c11x_fwdgt.h \
+ ../system/inc/gd32c11x/gd32c11x_gpio.h \
+ ../system/inc/gd32c11x/gd32c11x_i2c.h \
+ ../system/inc/gd32c11x/gd32c11x_pmu.h \
+ ../system/inc/gd32c11x/gd32c11x_bkp.h \
+ ../system/inc/gd32c11x/gd32c11x_rtc.h \
+ ../system/inc/gd32c11x/gd32c11x_spi.h \
+ ../system/inc/gd32c11x/gd32c11x_timer.h \
+ ../system/inc/gd32c11x/gd32c11x_usart.h \
+ ../system/inc/gd32c11x/gd32c11x_wwdgt.h \
+ ../system/inc/gd32c11x/gd32c11x_misc.h \
+ ../system/inc/gd32c11x/gd32c11x_exmc.h
+../system/inc/gd32c11x/gd32c11x_crc.h:
+../system/inc/cmsis/gd32c11x.h:
+../system/inc/cmsis/core_cm4.h:
+../system/inc/cmsis/core_cmInstr.h:
+../system/inc/cmsis/core_cmFunc.h:
+../system/inc/cmsis/core_cm4_simd.h:
+../system/inc/cmsis/system_gd32c11x.h:
+../system/inc/cmsis/gd32c11x_libopt.h:
+../system/inc/gd32c11x/gd32c11x_rcu.h:
+../system/inc/gd32c11x/gd32c11x_adc.h:
+../system/inc/gd32c11x/gd32c11x_can.h:
+../system/inc/gd32c11x/gd32c11x_ctc.h:
+../system/inc/gd32c11x/gd32c11x_dac.h:
+../system/inc/gd32c11x/gd32c11x_dbg.h:
+../system/inc/gd32c11x/gd32c11x_dma.h:
+../system/inc/gd32c11x/gd32c11x_exti.h:
+../system/inc/gd32c11x/gd32c11x_fmc.h:
+../system/inc/gd32c11x/gd32c11x_fwdgt.h:
+../system/inc/gd32c11x/gd32c11x_gpio.h:
+../system/inc/gd32c11x/gd32c11x_i2c.h:
+../system/inc/gd32c11x/gd32c11x_pmu.h:
+../system/inc/gd32c11x/gd32c11x_bkp.h:
+../system/inc/gd32c11x/gd32c11x_rtc.h:
+../system/inc/gd32c11x/gd32c11x_spi.h:
+../system/inc/gd32c11x/gd32c11x_timer.h:
+../system/inc/gd32c11x/gd32c11x_usart.h:
+../system/inc/gd32c11x/gd32c11x_wwdgt.h:
+../system/inc/gd32c11x/gd32c11x_misc.h:
+../system/inc/gd32c11x/gd32c11x_exmc.h:
